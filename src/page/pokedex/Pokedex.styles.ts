ef8ae6854c326158741008b215330bc0c782224c
llddang <bodyness@naver.com>
2025-02-04 20:30:59 +0900

    design: 포켓몬 상세 페이지의 디자인 구현
    #8

---
+++ src/page/pokedex/Pokedex.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PokedexContainer = styled.div`
+  max-width: 1200px;
+  margin: auto;
+  padding: 30px;
+  > *:not(:last-child) {
+    margin-bottom: 30px;
+  }
+`;
