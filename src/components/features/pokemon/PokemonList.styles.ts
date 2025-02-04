b6dd4c732aaa7d94742ca608fa0149e73f5e07e2
llddang <bodyness@naver.com>
2025-02-04 17:17:08 +0900

    feat: 포케몬 리스트 컴포넌트 구현

---
+++ src/components/features/pokemon/PokemonList.styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  min-width: 305px;
+  background-color: #f8f8f8;
+  box-sizing: border-box;
+  padding: 20px;
+  border-radius: 30px;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  justify-items: center;
+  gap: 20px;
+  background-color: #f0f0f0;
+  padding: 20px;
+  border: 1px solid #dddddd;
+  border-radius: 10px;
+`;
+
+const PokemonListS = {
+  Container,
+};
+export default PokemonListS;
