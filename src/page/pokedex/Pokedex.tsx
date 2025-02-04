af60c5498e31eb877cc5bf1a667baaa9e08ab5fa
llddang <bodyness@naver.com>
2025-02-04 17:17:43 +0900

    feat: PropDrilling으로 포켓몬 도감 페이지 구현

--- src/page/pokedex/Pokedex.tsx
+++ src/page/pokedex/Pokedex.tsx
@@ -1,3 +1,25 @@
+import PokemonDashboard from "@/components/features/pokemon/PokemonDashboard";
+import PokemonList from "@/components/features/pokemon/PokemonList";
+import { useState } from "react";
+import styled from "styled-components";
+
 export default function Pokedex() {
-  return <h1>Pokedex</h1>;
+  const [myPokemonIds, setMyPokemonIds] = useState<number[]>([]);
+
+  return (
+    <PokedexContainer>
+      <PokemonDashboard
+        pokemonIds={myPokemonIds}
+        setPokemonIdx={setMyPokemonIds}
+      />
+      <PokemonList pokemonIds={myPokemonIds} setPokemonIdx={setMyPokemonIds} />
+    </PokedexContainer>
+  );
 }
+
+const PokedexContainer = styled.div`
+  padding: 30px;
+  > *:not(:last-child) {
+    margin-bottom: 30px;
+  }
+`;
