75be8075f02de5a6a5eeffbc00c21e5fee2ed968
llddang <bodyness@naver.com>
2025-02-04 11:33:33 +0900

    feat: styled-component의 theme 설정

---
+++ src/styles/theme.config.ts
@@ -0,0 +1,36 @@
+export const breakpoints = {
+  mobile: "320px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "1440px",
+};
+
+export const fontSize = {
+  mobile: {
+    xs: "0.625rem",
+    sm: "0.70rem",
+    md: "0.75rem",
+    lg: "1rem",
+    xl: "2.125rem",
+  },
+  tablet: {
+    xs: "0.70rem",
+    sm: "0.75rem",
+    md: "0.875rem",
+    lg: "1.25rem",
+    xl: "2.25rem",
+  },
+  laptop: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.75rem",
+    xl: "2.5rem",
+  },
+};
+
+export const borderRadius = {
+  sm: "10px",
+  md: "20px",
+  lg: "30px",
+};
