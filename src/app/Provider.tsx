195daf6613bed1083091179fde6f9522e4c7a012
llddang <bodyness@naver.com>
2025-02-06 15:46:33 +0900

    feat: redux provider 설정
    #15

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,10 +1,16 @@
+import store from "@/store/redux";
 import { theme } from "@/styles/theme";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 
 interface ProviderProps {
   children: React.ReactNode;
 }
 
-export default function Provider({ children }: ProviderProps) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+export default function CustomProvider({ children }: ProviderProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>{children}</Provider>
+    </ThemeProvider>
+  );
 }
