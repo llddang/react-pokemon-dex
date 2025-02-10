bdbf1528222a0936bf61099961a8df1bafe6f585
llddang <bodyness@naver.com>
2025-02-10 10:09:49 +0900

    feat: react-toastify 사용할 수 있도록 초기설정
    #20

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import App from "@/App.tsx";
 import Provider from "@/app/Provider";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
       <App />
+      <ToastContainer />
     </Provider>
   </StrictMode>
 );
