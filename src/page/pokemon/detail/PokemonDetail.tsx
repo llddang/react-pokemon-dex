05ef86aacbefa2ff95e2d4b50d660bb8822745f3
llddang <bodyness@naver.com>
2025-02-10 17:46:41 +0900

    feat: 포켓몬 상세 페이지에 변경된 버튼 컴포넌트 적용
    #26

--- src/page/pokemon/detail/PokemonDetail.tsx
+++ src/page/pokemon/detail/PokemonDetail.tsx
@@ -1,22 +1,15 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
 import Typography from "@/components/common/Typography";
 import ButtonLink from "@/components/common/ButtonLink";
-import Button from "@/components/common/Button";
 import PokemonDetailS from "@/page/pokemon/detail/PokemonDetail.styled";
-import { addPokemon, deletePokemon } from "@/store/pokemons.slice";
-import { RootState } from "@/store/redux";
 import { formatNumber } from "@/lib/utils/format.util";
-import { MAX_POKEMON_COUNT } from "@/constants";
 import { POKEMON_DATA } from "@/mocks";
+import PokemonActionButtons from "@/components/features/pokemon-detail/PokemonActionButtons";
 
 export default function PokemonDetail() {
   const pokemonId = Number(useParams().id);
   const pokemon = POKEMON_DATA.find((pokemon) => pokemon.id === pokemonId);
-  const chosePokemons = useSelector((state: RootState) => state.pokemons);
-  const dispatch = useDispatch();
   const maxId = POKEMON_DATA.at(-1)?.id || 0;
 
   const navigate = useNavigate();
@@ -24,24 +17,6 @@ export default function PokemonDetail() {
   if (!pokemon) {
     navigate(-1);
     return null;
-  }
-  const isChose = chosePokemons.some((cp) => cp.id === pokemon.id);
-
-  function handleActionPokemonClick() {
-    if (!pokemon) return;
-    if (isChose) {
-      dispatch(deletePokemon(pokemon.id));
-      return toast.info(`${pokemon.name} 아(야), 잘 가!`);
-    }
-    if (chosePokemons.length >= MAX_POKEMON_COUNT)
-      return toast.error("더 이상 선택할 수 없습니다.");
-    dispatch(addPokemon(pokemon));
-    return toast.info(`와! ${pokemon.name} 을(를) 포획했다!`);
-  }
-
-  function handleDexLinkButtonClick(e: React.MouseEvent<HTMLAnchorElement>) {
-    e.preventDefault();
-    navigate("/pokemon", { state: { focusedId: pokemonId } });
   }
 
   return (
@@ -77,14 +52,7 @@ export default function PokemonDetail() {
         </ButtonLink>
       </PokemonDetailS.Body>
       <Typography>{pokemon.description}</Typography>
-      <PokemonDetailS.Buttons>
-        <Button onClick={handleActionPokemonClick}>
-          {isChose ? "제거하기" : "추가하기"}
-        </Button>
-        <ButtonLink onClick={handleDexLinkButtonClick} $variant="outline">
-          뒤로가기
-        </ButtonLink>
-      </PokemonDetailS.Buttons>
+      <PokemonActionButtons pokemonId={pokemon.id} />
     </PokemonDetailS.Container>
   );
 }
