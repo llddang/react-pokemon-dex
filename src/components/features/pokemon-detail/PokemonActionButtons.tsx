df6f9cab0cb1725086d1e235ea3f4a5d522f815a
llddang <bodyness@naver.com>
2025-02-10 17:44:59 +0900

    feat: 성능 개선을 위해 포켓몬 상세 페이지에서 버튼만 따로 관리
    #26

---
+++ src/components/features/pokemon-detail/PokemonActionButtons.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "@/components/common/Button";
+import ButtonLink from "@/components/common/ButtonLink";
+import usePokemonsStore from "@/lib/hooks/usePokemonsStore";
+
+interface PokemonActionButtonsProps {
+  pokemonId: number;
+}
+
+export default function PokemonActionButtons({
+  pokemonId,
+}: PokemonActionButtonsProps) {
+  const { isChose, handlePokemonStoreAction } = usePokemonsStore();
+
+  const navigate = useNavigate();
+  function handleDexLinkButtonClick(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    navigate("/pokemon", { state: { focusedId: pokemonId } });
+  }
+
+  return (
+    <Buttons>
+      <Button onClick={() => handlePokemonStoreAction(pokemonId)}>
+        {isChose(pokemonId) ? "제거하기" : "추가하기"}
+      </Button>
+      <ButtonLink onClick={handleDexLinkButtonClick} $variant="outline">
+        뒤로가기
+      </ButtonLink>
+    </Buttons>
+  );
+}
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
