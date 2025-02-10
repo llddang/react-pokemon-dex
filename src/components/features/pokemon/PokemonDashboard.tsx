b7752bf977dd99541521f9e132d99d33d5534719
llddang <bodyness@naver.com>
2025-02-10 18:40:51 +0900

    refactor: 포켓몬 대시보드에서 focusedId를 인자가 아닌 redux의 상태로 받아오도록 수정
    #26

--- src/components/features/pokemon/PokemonDashboard.tsx
+++ src/components/features/pokemon/PokemonDashboard.tsx
@@ -1,51 +1,26 @@
-import React, { SetStateAction } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
+import React from "react";
 import PokemonDashboardS from "@/components/features/pokemon/PokemonDashboard.styled";
 import PokemonFocusedCard from "@/components/features/pokemon/PokemonFocusedCard";
 import ButtonLink from "@/components/common/ButtonLink";
 import Button from "@/components/common/Button";
-import { RootState } from "@/store/redux";
-import { addPokemon, deletePokemon } from "@/store/pokemons.slice";
 import { MAX_POKEMON_COUNT } from "@/constants";
-import { POKEMON_DATA } from "@/mocks";
+import usePokemonsStore from "@/lib/hooks/usePokemonsStore";
+import useFocusedPokemonStore from "@/lib/hooks/useFocusedPokemonStore";
 
-export interface PokemonDashboardProps {
-  focusedId: number;
-  setFocusedId: React.Dispatch<SetStateAction<number>>;
-}
+export default function PokemonDashboard() {
+  const { chosePokemons, togglePokemon, isChose } = usePokemonsStore();
+  const { focusedPokemon, updateFocusedPokemon } = useFocusedPokemonStore();
 
-export default function PokemonDashboard({
-  focusedId,
-  setFocusedId,
-}: PokemonDashboardProps) {
-  const dispatch = useDispatch();
-
-  const chosePokemons = useSelector((state: RootState) => state.pokemons);
   const restPokemons = Array.from(
     { length: MAX_POKEMON_COUNT - chosePokemons.length },
     (_, i) => i
   );
-  const focusedPokemon = POKEMON_DATA.find((p) => p.id === focusedId);
-  const isChose = chosePokemons.some((cp) => cp.id === focusedId);
 
   function handleChosePokemonHover(e: React.MouseEvent<HTMLDivElement>) {
     const chosePokemon = (e.target as HTMLElement).closest("a");
     if (!chosePokemon) return;
     const pokemonId = Number(chosePokemon.id.split("-").at(-1));
-    setFocusedId(pokemonId);
-  }
-
-  function handleActionPokemonClick() {
-    if (!focusedPokemon) return;
-    if (isChose) {
-      dispatch(deletePokemon(focusedPokemon.id));
-      return toast.info(`${focusedPokemon.name} 아(야), 잘 가!`);
-    }
-    if (chosePokemons.length >= MAX_POKEMON_COUNT)
-      return toast.error("더 이상 선택할 수 없습니다.");
-    dispatch(addPokemon(focusedPokemon));
-    return toast.info(`와! ${focusedPokemon.name} 을(를) 포획했다!`);
+    updateFocusedPokemon(pokemonId);
   }
 
   return (
@@ -61,7 +36,7 @@ export default function PokemonDashboard({
               <img src={p.imageUrl} alt={p.name} />
             </PokemonDashboardS.PokemonLink>
           ))}
-          {restPokemons.reverse().map((key) => (
+          {restPokemons.map((key) => (
             <PokemonDashboardS.EmptyImg
               key={key}
               src="/pokemon_ball.png"
@@ -70,15 +45,18 @@ export default function PokemonDashboard({
           ))}
         </PokemonDashboardS.GridBox>
         <PokemonDashboardS.Buttons>
-          <Button onClick={handleActionPokemonClick}>
-            {isChose ? "제거하기" : "추가하기"}
+          <Button onClick={() => togglePokemon(focusedPokemon.id)}>
+            {isChose(focusedPokemon.id) ? "제거하기" : "추가하기"}
           </Button>
-          <ButtonLink href={`/pokemon/detail/${focusedId}`} $variant="outline">
+          <ButtonLink
+            href={`/pokemon/detail/${focusedPokemon.id}`}
+            $variant="outline"
+          >
             상세보기
           </ButtonLink>
         </PokemonDashboardS.Buttons>
       </div>
-      <PokemonFocusedCard focusedId={focusedId} />
+      <PokemonFocusedCard focusedId={focusedPokemon.id} />
     </PokemonDashboardS.Container>
   );
 }
--- src/page/pokemon/Pokemon.tsx
+++ src/page/pokemon/Pokemon.tsx
@@ -1,21 +1,16 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
 import PokemonPageS from "@/page/pokemon/Pokemon.styled";
 import PokemonList from "@/components/features/pokemon/PokemonList";
 import PokemonDashboard from "@/components/features/pokemon/PokemonDashboard";
 import PokemonUsageGuideModal from "@/components/features/pokemon/PokemonUsageGuideModal";
 
 export default function Pokemon() {
-  const { state } = useLocation();
-  const [focusedId, setFocusedId] = useState<number>(state?.focusedId ?? 1);
-
   return (
     <>
       <PokemonPageS.Container>
         <PokemonPageS.Title>choose your Pokémon</PokemonPageS.Title>
         <hr />
-        <PokemonList focusedId={focusedId} setFocusedId={setFocusedId} />
-        <PokemonDashboard focusedId={focusedId} setFocusedId={setFocusedId} />
+        <PokemonList />
+        <PokemonDashboard />
       </PokemonPageS.Container>
       <PokemonUsageGuideModal />
     </>
